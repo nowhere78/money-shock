5bde0d4ff4834e3178c2fb53e5dae8900cb6eb10
nowhere78 <wide1002@gmail.com>
2026-05-25 19:54:40 +0900

    feat: add PWA support for PWABuilder

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'money-shock-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.svg',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
+  'https://fonts.googleapis.com/css2?family=Pretendard:wght@300;400;500;600;700;800;900&family=Noto+Sans+KR:wght@400;700;900&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
