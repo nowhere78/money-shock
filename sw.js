89dc883b4b4927292108d8c21fd79b08358f866a
nowhere78 <wide1002@gmail.com>
2026-05-25 22:03:24 +0900

    feat: add PNG icons to PWA manifest and cache

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'money-shock-v1';
+const CACHE_NAME = 'money-shock-v2';
 const ASSETS = [
   './',
   './index.html',
   './icon.svg',
+  './images/icon-192.png',
+  './images/icon-512.png',
   './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js',
   'https://fonts.googleapis.com/css2?family=Pretendard:wght@300;400;500;600;700;800;900&family=Noto+Sans+KR:wght@400;700;900&display=swap'
